5e255d069aa12ecedf8b55283ba0d43050eae8a4
Suiam Costa <suiamcosta@hotmail.com>
2020-01-22 20:10:41 -0300

    semana omnistack week9

---
+++ backend/src/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-klalq.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } =  socket.handshake.query;
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req,res,next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+
+});
+
+//GET,POST,PUT,DELETE
+//req.query = acessar query params (para filtros)
+//req.params = acessar route params (para edição e delete)
+//req.body = acessar corpo da requisição (para criação, edição)
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+server.listen(3333);
